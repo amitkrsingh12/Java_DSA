344544f657d531bfd763ad2b6d5692924ed45929
severusamit <severusamit1212@gmail.com>
2024-08-11 18:20:33 +0530

    Use of Final Keyword

--- src/AnitaSethBook/Methods/FinalAttribute.java
+++ src/AnitaSethBook/Methods/FinalAttribute.java
@@ -1,2 +1,17 @@
-package AnitaSethBook.Methods;public class FinalAttribute {
+package AnitaSethBook.Methods;
+
+public class FinalAttribute
+{
+    final int marks=80;
+    void run()
+    {
+        // cannot reassign final
+      //  marks =60;
+        System.out.println("Subject Cleared");
+    }
+    public static void main(String args[])
+    {
+        FinalAttribute obj = new FinalAttribute();
+        obj.run();
+    }
 }
