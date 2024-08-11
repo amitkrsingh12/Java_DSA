2886352b63e9b865b015875efcf0d17f25083209
severusamit <severusamit1212@gmail.com>
2024-08-11 18:21:10 +0530

    Getters and Setters

--- src/AnitaSethBook/Methods/GettersandSetters.java
+++ src/AnitaSethBook/Methods/GettersandSetters.java
@@ -1,2 +1,20 @@
-package AnitaSethBook.Methods;public class GettersandSetters {
+package AnitaSethBook.Methods;
+
+public class GettersandSetters
+{
+    private String EmployeeName;
+    String getName()
+    {
+        return EmployeeName;
+    }
+    public void setName(String str)
+    {
+        EmployeeName=str;
+    }
+    public static void main(String args[])
+    {
+        GettersandSetters obj = new GettersandSetters();
+        obj.setName("Amit");
+        System.out.println(obj.getName());
+    }
 }
