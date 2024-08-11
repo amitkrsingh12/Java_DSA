22dd5af2d9018c0fcf8640ea6d0ac63791c8fa79
severusamit <severusamit1212@gmail.com>
2024-08-11 18:21:52 +0530

    Factorial using Recursion

--- src/AnitaSethBook/Methods/RecurseiveFactorial.java
+++ src/AnitaSethBook/Methods/RecurseiveFactorial.java
@@ -1,2 +1,28 @@
-package AnitaSethBook.Methods;public class RecurseiveFactorial {
+package AnitaSethBook.Methods;
+import java.util.Scanner;
+
+class Fact
+{
+    int factorial(int n)
+    {
+        int f=1;
+        if(n==1)
+        {
+            return n;
+        }
+        f=n*factorial(n-1);
+        return f;
+    }
 }
+public class RecurseiveFactorial
+{
+    public static void main(String args[])
+    {
+        Fact obj = new Fact();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number whose factoiral you want ?");
+        int n=in.nextInt();
+        System.out.println(obj.factorial(n));
+    }
+}
+
