56d4916887a5385b126f121c85a7f1d9b4ad36f2
severusamit <severusamit1212@gmail.com>
2024-08-11 18:22:38 +0530

    Fibonacci Series Calculation using Recursion

--- src/AnitaSethBook/Methods/RecursiveFibonacci.java
+++ src/AnitaSethBook/Methods/RecursiveFibonacci.java
@@ -1,2 +1,26 @@
-package AnitaSethBook.Methods;public class RecursiveFibonacci {
+package AnitaSethBook.Methods;
+
+import java.util.Scanner;
+class Fibonacci{
+    int fibonacci(int n)
+    {
+        if(n==0||n<2)
+        {
+            return n;
+        }else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+}
+public class RecursiveFibonacci
+{
+    public static void main(String args[])
+    {
+
+        Fibonacci obj = new Fibonacci();
+        for(int i=0;i<10;i++)
+        {
+            System.out.print(obj.fibonacci(i)+" ");
+        }
+    }
 }
