c0a4104af28c46099c86e978084d1c5e2e91ff5f
severusamit <severusamit1212@gmail.com>
2024-08-12 13:08:07 +0530

    Creates an object of class and do the basic implementation

--- src/OOP/ClassandObject/StudentUse.java
+++ src/OOP/ClassandObject/StudentUse.java
@@ -1,2 +1,19 @@
-package OOP.ClassandObject;public class StudentUse {
+package OOP.ClassandObject;
+
+public class StudentUse
+{
+    public static void main(String args[])
+    {
+        Student s1 = new Student();
+        System.out.println(s1);
+        Student s2 = new Student();
+
+        s1.name ="Manisha";
+        s1.rollNumber=10;
+        System.out.println(s1.name + " "+s1.rollNumber);
+
+        s2.name="Ankush";
+        s2.rollNumber=50;
+        System.out.println(s2.name +" "+s2.rollNumber);
+    }
 }
