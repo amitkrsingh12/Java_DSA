98fc4a1ba9f58f952f40216f9a2ceb3c442b603c
severusamit <severusamit1212@gmail.com>
2024-08-13 21:49:16 +0530

    Use of Static Keyword

--- src/OOP/Constructor/Student.java
+++ src/OOP/Constructor/Student.java
@@ -6,19 +6,22 @@ public class Student
     double cgpa;
     final private int rollNumber;
     final double conversionFactor =0.95;
+    static int numStudents;
     public Student(String name,int rollNumber)
     {
         this.name=name;
-        this.rollNumber=rollNumber;
+        numStudents++;
+        this.rollNumber=numStudents;
     }
 //    public Student(String n)
 //    {
 //        name=n;
 //    }
-//    public Student(){
-//        name="abc";
-//        rollNumber=10;
-//    }
+    public Student(){
+        name="abc";
+        numStudents++;
+        rollNumber=numStudents;
+    }
     public void print()
     {
         System.out.println(name + " "+rollNumber);
