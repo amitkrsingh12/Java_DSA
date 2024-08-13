08f9ee43ce03c78f4f43c304d51364ffd6a07d64
severusamit <severusamit1212@gmail.com>
2024-08-13 20:40:46 +0530

    Here we call Student class

--- src/OOP/Constructor/StudentUse.java
+++ src/OOP/Constructor/StudentUse.java
@@ -1,2 +1,12 @@
-package OOP.Constructor;public class StudentUse {
+package OOP.Constructor;
+
+public class StudentUse {
+    public static void main(String args[])
+    {
+        Student s1 = new Student("Amit",10);
+
+        s1.print();
+        Student s2 = new Student();
+        s2.print();
+    }
 }
