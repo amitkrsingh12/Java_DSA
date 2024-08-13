b3a6a5da00aaff13d0e57fbdab2337b10822a1c0
severusamit <severusamit1212@gmail.com>
2024-08-13 21:49:48 +0530

    Functionalities of Static Keyword

--- src/OOP/Constructor/StudentUse.java
+++ src/OOP/Constructor/StudentUse.java
@@ -3,10 +3,12 @@ package OOP.Constructor;
 public class StudentUse {
     public static void main(String args[])
     {
+        System.out.println(Student.numStudents);
         Student s1 = new Student("Amit",10);
 
-        s1.print();
+    //    s1.print();
         Student s2 = new Student("Pratiuk",20);
-        s2.print();
+    //    s2.print();
+        System.out.println(Student.numStudents);
     }
 }
