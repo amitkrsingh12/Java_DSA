a1526cce411fe3cb009c516fabe705a15906b864
severusamit <severusamit1212@gmail.com>
2024-08-14 13:41:10 +0530

    Private Modifiers

--- src/OOP/Inheritance/Car.java
+++ src/OOP/Inheritance/Car.java
@@ -2,4 +2,7 @@ package OOP.Inheritance;
 
 public class Car extends Vehicle {
     int numDoors;
+    public void printCar(){
+        System.out.println("Car "+ "color : "+getColor()+ " maxSpeed "+ maxSpeed +" numDoors : "+numDoors);
+    }
 }
