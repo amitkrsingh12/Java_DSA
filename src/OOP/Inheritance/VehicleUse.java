8c7f0381bf340cf3343a6cfd7b1048854155267f
severusamit <severusamit1212@gmail.com>
2024-08-14 13:51:39 +0530

    Function Overriding Implementation

--- src/OOP/Inheritance/VehicleUse.java
+++ src/OOP/Inheritance/VehicleUse.java
@@ -4,21 +4,23 @@ public class VehicleUse {
     public static void main(String args[])
     {
         Vehicle v = new Vehicle();
-    //    v.color="Red";
-        v.setColor("red");
+        v.setColor("Red");
         v.maxSpeed=80;
         v.print();
-    //    v.print();
 
         Car c = new Car();
-    //    c.color="Black";
         c.setColor("Black");
         c.maxSpeed=100;
         c.numDoors=4;
-    //    c.print();
-        c.printCar();
+        /*
+        We have two print function. 1 in Parent Class(Vehicle) and the other in  Child Class(Car)
+        So the below c.print() check first in car class, and implements it.
+        Function overriding in Java is a feature that allows a subclass to provide a specific
+        implementation of a method that is already defined by its superclass. When a method in a
+        subclass has the same name,  return type, and parameters as a method in its superclass the
+        method in the subclass overrides the one in the superclass.
+         */
+        c.print();
 
-        Bicycle b = new Bicycle();
-        b.print();
     }
 }
