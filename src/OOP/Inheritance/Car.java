6e275b741202726afc6c191a988b6a8e9d8fd1a3
severusamit <severusamit1212@gmail.com>
2024-08-14 13:50:55 +0530

    Function Overriding Implementation

--- src/OOP/Inheritance/Car.java
+++ src/OOP/Inheritance/Car.java
@@ -2,7 +2,7 @@ package OOP.Inheritance;
 
 public class Car extends Vehicle {
     int numDoors;
-    public void printCar(){
+    public void print(){
         System.out.println("Car "+ "color : "+getColor()+ " maxSpeed "+ maxSpeed +" numDoors : "+numDoors);
     }
 }
