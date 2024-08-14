490d703b98662589b4bbf7b2be42a7911fe88201
severusamit <severusamit1212@gmail.com>
2024-08-14 13:32:19 +0530

    Parent Class

--- src/OOP/Inheritance/Vehicle.java
+++ src/OOP/Inheritance/Vehicle.java
@@ -1,2 +1,9 @@
-package OOP.Inheritance;public class Vehicle {
+package OOP.Inheritance;
+
+public class Vehicle {
+    String color;
+    int maxSpeed;
+    public void print(){
+        System.out.println("Vehicle " + "color "+color +" maxSpeed : "+maxSpeed);
+    }
 }
