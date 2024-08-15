8154f438f08021701c1511f779236193a93b9a3f
severusamit <severusamit1212@gmail.com>
2024-08-15 12:00:40 +0530

    Update

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -12,13 +12,24 @@ public class Factorial {
         }
         return n*fact(n-1);
     }
-    public static void main(String argsp[])
+
+    public static int arraySum(int[] input) {
+        int n = input.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] smaller = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            smaller[i] = input[i];
+        }
+        return arraySum(smaller) + input[n - 1];
+    }
+    public static void main(String args[])
     {
         Scanner in =  new Scanner(System.in);
-        int n;
-        System.out.println("Enter the number whose factorial you want ?");
-        n=in.nextInt();
-        System.out.println(fact(n));
 
+        System.out.println("Enter the number whose factorial you want ?");
+        int n=in.nextInt();
+        System.out.println("Factorial Function "+fact(n));
     }
 }
