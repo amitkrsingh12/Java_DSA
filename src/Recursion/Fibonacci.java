bfd1d5449e9d98f85a6166968968570683abfd56
severusamit <severusamit1212@gmail.com>
2024-08-15 18:20:45 +0530

    Fibonacci of a number

--- src/Recursion/Fibonacci.java
+++ src/Recursion/Fibonacci.java
@@ -1,2 +1,22 @@
-package Recursion;public class Fibonacci {
+package Recursion;
+
+import java.util.Scanner;
+
+public class Fibonacci
+{
+    public static int fib(int n)
+    {
+        if(n==1 || n==2)
+        {
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println(fib(20));
+    }
 }
