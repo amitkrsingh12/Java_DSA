54e1c3795cf1fe4f8e5b38e848b56587c1ec0c68
severusamit <severusamit1212@gmail.com>
2024-08-15 12:01:12 +0530

    Count the sum of digits of a number using recursion

--- src/Recursion/countDigits.java
+++ src/Recursion/countDigits.java
@@ -1,2 +1,27 @@
-package Recursion;public class countDigits {
+package Recursion;
+
+import java.util.Scanner;
+
+public class countDigits
+{
+    public static int countDigits(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        return 1 + countDigits(n/10);
+    }
+    public static void main(String argsp[])
+    {
+        Scanner in =  new Scanner(System.in);
+        System.out.println("Enter digits whose sum you want to count ?");
+        int x=in.nextInt();
+        System.out.println("Count Digits : "+countDigits(x));
+
+
+
+
+
+    }
 }
