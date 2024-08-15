739e815086cf52d13cac36e5ff4120651e582843
severusamit <severusamit1212@gmail.com>
2024-08-15 18:20:17 +0530

    Replace the word pi in a string with 3.14

--- src/Recursion/ReplacePi.java
+++ src/Recursion/ReplacePi.java
@@ -1,2 +1,24 @@
-package Recursion;public class ReplacePi {
+package Recursion;
+
+public class ReplacePi
+{
+    public static String replace(String n)
+    {
+        if(n.length()==0)
+        {
+            return n;
+        }
+        if(n.charAt(0)=='p' && n.charAt(1)=='i')
+        {
+            String smallOutput = replace(n.substring(2));
+            return 3.14 +smallOutput;
+        }else{
+            String smallOutput = replace(n.substring(1));
+            return n.charAt(0)+smallOutput;
+        }
+    }
+    public static void main(String args[])
+    {
+        System.out.println(replace("piamitsinghpi"));
+    }
 }
