7d17531ee5cbb5cb82cdf5e1f728a6ddee4b1314
severusamit <severusamit1212@gmail.com>
2024-08-15 18:19:42 +0530

    Count zeros in an integer

--- src/Recursion/CountZeros.java
+++ src/Recursion/CountZeros.java
@@ -1,2 +1,30 @@
-package Recursion;public class CountZeros {
+package Recursion;
+
+import java.util.Scanner;
+
+public class CountZeros
+{
+    public static int countZeros(int input)
+    {
+        if(input==0)
+        {
+            return 1;
+        }
+        if(input<10)
+        {
+            return 0;
+        }
+        if(input%10==0)
+        {
+            return 1 +countZeros(input/10);
+        }
+        return countZeros(input/10);
+    }
+    public static void main(String args[])
+    {
+        Scanner in= new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println(countZeros(n));
+
+    }
 }
