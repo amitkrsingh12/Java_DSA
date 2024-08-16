91fe902fcff791f53bd8e04c41f64801eff90e4b
severusamit <severusamit1212@gmail.com>
2024-08-16 21:28:45 +0530

    Gemoetric Sum (Given k, find the geometric sum )

--- src/Recursion/Practice.java
+++ src/Recursion/Practice.java
@@ -25,7 +25,7 @@ public class Practice {
     {
         if(n<0)
         {
-            return 0;
+            return 1;
         }
         if(n==0)
         {
@@ -33,26 +33,45 @@ public class Practice {
         }
         return staircase(n-1) +staircase(n-2) +staircase(n-3);
     }
+    public static double  gemoetricSum(int input)
+    {
+        /*
+        Given k, find the geometric sum i.e.
+        1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k)
+         */
+        if(input==0)
+        {
+            return 1;
+        }
+        return 1/Math.pow(2,input) + gemoetricSum(input-1);
+    }
 
 
     public static void main(String args[])
     {
         Scanner in = new Scanner(System.in);
-        System.out.println("------------------------------------------------------------");
-        System.out.println("Enter two numbers for multiplication ?");
-        int m=in.nextInt();
-        int n=in.nextInt();
-        System.out.println("Multiplication using Recursion : " +multiplication(m,n));
+//        System.out.println("------------------------------------------------------------");
+//        System.out.println("Enter two numbers for multiplication ?");
+//        int m=in.nextInt();
+//        int n=in.nextInt();
+//        System.out.println("Multiplication using Recursion : " +multiplication(m,n));
+//
+//        System.out.println("------------------------------------------------------------");
+//        System.out.println("Enter a number whose sum of digits you want ?");
+//        int o = in.nextInt();
+//        System.out.println("Sum of digits of "+ o + " :" +sumOfDigits(o));
+//
+//        System.out.println("------------------------------------------------------------");
+//        System.out.println("Enter no of stairs ?");
+//        int p =in.nextInt();
+//        System.out.println("Minimum no of steps required is : "+staircase(p));
 
         System.out.println("------------------------------------------------------------");
-        System.out.println("Enter a number whose sum of digits you want ?");
-        int o = in.nextInt();
-        System.out.println("Sum of digits of "+ o + " :" +sumOfDigits(o));
-        
-        System.out.println("------------------------------------------------------------");
-        System.out.println("Enter no of stairs ?");
-        int p =in.nextInt();
-        System.out.println("Minimum no of steps required is : "+staircase(p));
+        System.out.println("enter a number whose geometric sum you want ? ");
+        int q =in.nextInt();
+        System.out.println("Geometric sum : " +gemoetricSum(q));
+
+
 
 
 
