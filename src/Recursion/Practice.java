9725ff36db98a5c05dd5ef76f5e90d4d9e4363c6
severusamit <severusamit1212@gmail.com>
2024-08-16 21:38:32 +0530

    Suppose you have a string, S, made up of only 'a's and 'b's.         Write a recursive function that checks if the string was generated using         the following rules:         a. The string begins with an 'a'         b. Each 'a' is followed by nothing or an 'a' or bb         c. Each bb is followed by nothing or an 'a'         If all the rules are followed by the given string, return true otherwise return false.

--- src/Recursion/Practice.java
+++ src/Recursion/Practice.java
@@ -57,6 +57,33 @@ public class Practice {
         }
         return isPalindrome(str.substring(1,str.length()-1));
     }
+    public static boolean checkAB(String str )
+    {
+        /*
+        Suppose you have a string, S, made up of only 'a's and 'b's.
+        Write a recursive function that checks if the string was generated using
+        the following rules:
+        a. The string begins with an 'a'
+        b. Each 'a' is followed by nothing or an 'a' or "bb"
+        c. Each "bb" is followed by nothing or an 'a'
+        If all the rules are followed by the given string, return true otherwise return false.
+         */
+        if(str.length()==0)
+        {
+            return true;
+        }
+        if(str.charAt(0)=='a')
+        {
+            if(str.substring(1).length()>1 && str.substring(1,3).equals("bb"))
+            {
+                return checkAB(str.substring(3));
+            }
+            else{
+                return checkAB(str.substring(1));
+            }
+        }
+        return false;
+    }
 
 
     public static void main(String args[])
@@ -83,10 +110,15 @@ public class Practice {
 //        int q =in.nextInt();
 //        System.out.println("Geometric sum : " +gemoetricSum(q));
 
+//        System.out.println("------------------------------------------------------------");
+//        System.out.println("Enter string whose palindrome you want to search ?");
+//        String str =in.nextLine();
+//        System.out.println(isPalindrome(str));
+
         System.out.println("------------------------------------------------------------");
-        System.out.println("Enter string whose palindrome you want to search ?");
-        String str =in.nextLine();
-        System.out.println(isPalindrome(str));
+        System.out.println("Enter ab combination ?");
+        String str1 =in.nextLine();
+        System.out.println(checkAB(str1));
 
 
 
