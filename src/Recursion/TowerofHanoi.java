f99ec639f6e80185804a91cc64463ef66bddaece
severusamit <severusamit1212@gmail.com>
2024-08-20 17:02:05 +0530

    Tower of Hanoi Problem

--- src/Recursion/TowerofHanoi.java
+++ src/Recursion/TowerofHanoi.java
@@ -1,2 +1,23 @@
-package Recursion;public class TowerofHanoi {
+package Recursion;
+
+import java.util.Scanner;
+
+public class TowerofHanoi
+{
+    public static void towerHanoi(int n,char start ,char helper, char destination)
+    {
+        if(n==1){
+            System.out.println("Move 1st disk from " +start+ " to "+destination);
+            return;
+        }
+        towerHanoi(n-1,start,destination,helper);
+        System.out.println("Move " +n+ " th disk from "+start+" to " +destination);
+        towerHanoi(n-1,helper,start,destination);
+    }
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+       towerHanoi(n,'s','h','d');
+    }
 }
