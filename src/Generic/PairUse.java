2848dc6ef794240699d1c112b005078db5e087a5
severusamit <severusamit1212@gmail.com>
2024-08-21 10:07:11 +0530

    Uus Of Generic

--- src/Generic/PairUse.java
+++ src/Generic/PairUse.java
@@ -3,7 +3,15 @@ package Generic;
 public class PairUse{
     public static void main(String args[])
     {
-        
+        Pair<String,Integer> pSI = new Pair<>("abc",10);
+
+//        Pair<String> p = new Pair<>();
+//        System.out.println(p.getFirst() + " " + p.getSecond());
+//        Pair <String> ps = new Pair<String>("amit","singh");
+//        System.out.println(ps.getFirst() + " " + ps.getSecond());
+//
+//        Pair <Integer> pI = new Pair<Integer>(10,20);
+//        System.out.println(pI.getFirst() + " " + pI.getSecond());
     }
 
 }
