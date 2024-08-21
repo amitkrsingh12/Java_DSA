ebadbd978ac0b5b886288ac83da84b7cf8f43dab
severusamit <severusamit1212@gmail.com>
2024-08-21 19:56:57 +0530

    Removeal of commented code and implemented increment function

--- src/DataStructure/LinkedList/LinkedListUse.java
+++ src/DataStructure/LinkedList/LinkedListUse.java
@@ -10,26 +10,36 @@ public class LinkedListUse
         Node<Integer> n3 = new Node<>(30);
         Node<Integer> n4 = new Node<>(40);
         n1.next =n2;
-    //    System.out.println("n1 "+ n1 + " data " +n1.data + " next " +n1.next);
         n2.next=n3;
-    //    System.out.println("n2 "+ n2 + " data " +n2.data + " next " +n2.next);
         n3.next=n4;
-    //    System.out.println("n3 "+ n3 + " data " +n3.data + " next " +n3.next);
-    //    System.out.println("n4 "+ n4 + " data " +n4.data + " next " +n4.next);
         return n1;
     }
     public static void print(Node<Integer> head){
         System.out.println("Print : -------------");
-        while(head!=null){
-            System.out.print(head.data + " ");
-            head=head.next;
+        Node<Integer> temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + " ");
+            temp=temp.next;
         }
         System.out.println();
+    }
+    public static void increment(Node<Integer> head){
+        Node<Integer> temp = head;
+        while(temp!=null){
+            temp.data++;
+            temp=temp.next;
+        }
     }
     public static void main(String args[]){
 
         Node<Integer> head = createLinkedList();
+        increment(head);
         print(head);
+
+    //the below print function runs because it intializes head again
+    //    print(head);
+
+
 //        Node<Integer> n1 = new Node<>(10);
 //        System.out.println(n1);
 //        System.out.println(n1.data);
