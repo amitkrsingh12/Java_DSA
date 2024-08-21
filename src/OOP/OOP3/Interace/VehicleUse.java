5b46efcd927c812facbb8e68b3aa29d903703d0a
severusamit <severusamit1212@gmail.com>
2024-08-21 10:07:46 +0530

    update

--- src/OOP/OOP3/Interace/VehicleUse.java
+++ src/OOP/OOP3/Interace/VehicleUse.java
@@ -6,10 +6,10 @@ public class VehicleUse {
         Vehicle v = new Vehicle();
         VehicleInterface vI;
         //the below line throws error because now vehicle class does not implement Vehicle interface @1
-        vI =  new Vehicle();
+       // vI =  new Vehicle();
         v.print();
-        vI.getCompany();
-        vI.isMotorized();
+        //vI.getCompany();
+       // vI.isMotorized();
 
     }
 }
