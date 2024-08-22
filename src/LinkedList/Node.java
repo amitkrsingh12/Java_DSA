38bc5606cb0d7fddd8757340eac0b93fc2b7a89c
severusamit <severusamit1212@gmail.com>
2024-08-22 18:24:17 +0530

    Implementation of Node Class and Build Class

---
+++ src/LinkedList/Node.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class Node<T>
+{
+    T data;
+    Node<T> next;
+    Node(T data){
+        this.data=data;
+    //    next=null;        by default its value is set to null
+    }
+
+}
