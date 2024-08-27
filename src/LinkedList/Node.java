d8d459271db0f34d64f12873a071e9eed6a41612
severusamit <severusamit1212@gmail.com>
2024-08-27 11:59:38 +0530

    Updation of Node

--- src/LinkedList/Node.java
+++ src/LinkedList/Node.java
@@ -4,7 +4,9 @@ public class Node<T>
 {
     T data;
     Node<T> next;
-    Node(T data){
+
+    Node(T data)
+    {
         this.data=data;
     //    next=null;        by default its value is set to null
     }
